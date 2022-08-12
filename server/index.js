6e41646c8e09681b519adf1762001c0775a157f8
prizoner627 <dushyantha.world@gmail.com>
2022-08-13 01:36:15 +0530

    ii

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "http://34.142.253.123:3001"],
     credentials: true,
   })
 );
